f3e11636f19dc17385ded22b827dc28265b32634
niawjunior <pasupol@dome.cloud>
2017-12-20 13:34:27 +0700

    show todo list and create function handleAddTodo

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <ul v-for="todo in $data.todos" :key="todo.key">
+          <li>
+              {{ todo. text }}
+          </li>
+      </ul>
+      <form v-on:submit="handleAddTodo($event)">
+          <input type="text" v-model="input">
+      </form>
+  </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            handleAddTodo(e) {
+                e.preventDefault();
+                this.$data.todos.push({text:this.input, done:false});
+                this.input = "";
+            }
+        },
+        data() {
+            return this.$props.state;
+        },
+        props:['state']
+    }
+</script>
+
+<style>
+
+</style>
+
+
