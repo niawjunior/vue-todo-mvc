bc0156df45fed7edd37a8f03a93c9bc8defd9961
niawjunior <pasupol@dome.cloud>
2017-12-20 13:22:43 +0700

    create object vue and fake data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.info("HELLO WORLD");
+import Vue from 'vue';
+import App from './App';
+
+new Vue({
+    el: "#app",
+    components: { App },
+    template: `<App :state="state"/>`,
+    data: {
+        state: {
+            showDone: false,
+            todos:[{
+                text: "Refactor CSS",
+                done: true,
+            },{
+                text: "Secure Docker Instances",
+                done: false,
+            }, {
+                text: "Injdect flavoring",
+                dont: false
+            }
+          ]}
+        }
+})
